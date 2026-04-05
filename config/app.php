eb943ba7041bc17086ef01ea28ae8b363de68569
Vinicius Reinehr <viniciusreinehr@MacBook-Air-de-Vinicius-6.local>
2026-04-05 10:44:26 -0300

    fix: implement smart encryption detection (AES-128 or AES-256 auto-scaling)

--- config/app.php
+++ config/app.php
@@ -1,6 +1,19 @@
 <?php
 
+$key = env('APP_KEY');
+$cipher = 'AES-256-CBC';
+
+if ($key) {
+    $tempKey = $key;
+    if (str_starts_with($tempKey, 'base64:')) {
+        $tempKey = base64_decode(substr($tempKey, 7));
+    }
+    if (mb_strlen($tempKey, '8bit') === 16) {
+        $cipher = 'AES-128-CBC';
+    }
+}
+
 return [
     'key' => env('APP_KEY'),
-    'cipher' => env('APP_CIPHER', 'AES-128-CBC'),
+    'cipher' => env('APP_CIPHER', $cipher),
 ];
