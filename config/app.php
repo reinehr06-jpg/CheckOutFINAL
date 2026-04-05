a154b97d5f9d0352b482c99261268a6cfc964ef1
Vinicius Reinehr <viniciusreinehr@MacBook-Air-de-Vinicius-6.local>
2026-04-05 10:35:21 -0300

    fix: switch cipher to AES-128-CBC to match production APP_KEY length

--- config/app.php
+++ config/app.php
@@ -5,7 +5,7 @@ return [
 
     'key' => env('APP_KEY'),
 
-    'cipher' => 'AES-256-CBC',
+    'cipher' => 'AES-128-CBC',
 
     'maintenance' => [
         'driver' => env('APP_MAINTENANCE_DRIVER', 'file'),
